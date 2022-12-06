b2c7d53e2810004ca8c066b8453985ecd47081f1
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-12-06 21:02:55 +0100

    arg naming

--- src/calvano2020/stop.jl
+++ src/calvano2020/stop.jl
@@ -2,7 +2,7 @@ using ReinforcementLearning
 
 function CalvanoStop(calvano_params::CalvanoParams)
     ComposedStopCondition(
-        StopAfterEpisode(calvano_params.n_iter),
+        StopAfterEpisode(calvano_params.max_iter),
         StopAfterNoImprovement(
             () -> calvano_params.convergence_check.convergence_metric,
             calvano_params.convergence_threshold),
