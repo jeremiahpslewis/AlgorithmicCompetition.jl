2046a154abe994cf52e1cfb358517eafafc699ee
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-12-06 21:45:05 +0100

    Fix args

--- src/calvano2020/env.jl
+++ src/calvano2020/env.jl
@@ -27,7 +27,8 @@ Base.@kwdef mutable struct CalvanoEnv <: AbstractEnv
         n_players::Int,
         memory_length::Int,
         price_options::Base.AbstractVecOrTuple{Float64},
-        max_iter::Int, convergence_threshold::Int,
+        max_iter::Int,
+        convergence_threshold::Int,
         profit_function::Function,
 )
         # Special case starting conditions with 'missing' in lookbacks, think about best way of handling this...
@@ -36,9 +37,25 @@ Base.@kwdef mutable struct CalvanoEnv <: AbstractEnv
         price_index = 1:n_prices
         convergence_check = ConvergenceCheck(n_state_space=n_prices, n_players=n_players)
         init_matrix = zeros(n_prices, n_prices)
-        new(α, β, δ, n_players, memory_length, price_options, max_iter, convergence_threshold, n_prices, price_index, convergence_check, init_matrix,
-            fill(1, memory_length, n_players),
-            ntuple((i) -> false, n_players)
+        new(
+            α,
+            β,
+            δ,
+            n_players,
+            memory_length,
+            price_options,
+            max_iter,
+            convergence_threshold,
+            n_prices,
+            price_index,
+            convergence_check,
+            init_matrix,
+            profit_function,
+
+            fill(1, memory_length, n_players), # Memory
+            ntuple((i) -> false, n_players), # Is converged
+            (0.0, 0.0), # Reward
+            false, # Is done
         )
     end
 end
