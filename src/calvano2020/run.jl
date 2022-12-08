ee452854771d4c9f6adca82ddd28e98e12d03364
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-12-08 14:37:11 +0100

    Tweak run function

--- src/calvano2020/run.jl
+++ src/calvano2020/run.jl
@@ -1,19 +1,15 @@
 using ReinforcementLearning
 
-function runCalvano(env::CalvanoEnv)
-    calvano_experiment = Experiment(
-        policy = CalvanoPolicy(env),
-        env = SequentialEnv(env),
-        stop_condition = CalvanoStop(env),
-        hook = CalvanoHook(env),
+function setupCalvanoExperiment(env::CalvanoEnv)
+    Experiment(
+        policy=CalvanoPolicy(env),
+        env=SequentialEnv(env),
+        stop_condition=CalvanoStop(env),
+        hook=CalvanoHook(env),
     )
-    return run(calvano_experiment)
 end
 
-# Look into DistributedReinforcementLearning.jl for running grid of experiments
-# Add hook `(hook::YourHook)(::PostExperimentStage, agent, env)` to save profit results!
-
-function runCalvano(
+function setupCalvanoExperiment(
     α::Float64,
     β::Float64,
     δ::Float64,
@@ -23,15 +19,43 @@ function runCalvano(
     convergence_threshold::Int = Int(1e5),
 )
     env = CalvanoEnv(
-        α = α,
-        β = β,
-        δ = δ,
-        n_players = 2,
-        memory_length = 2,
-        price_options = price_options,
-        max_iter = max_iter,
-        convergence_threshold = convergence_threshold,
-        profit_function = (p_1, p_2) -> π_fun(p_1, p_2, competition_params),
+        α=α,
+        β=β,
+        δ=δ,
+        n_players=2,
+        memory_length=2,
+        price_options=price_options,
+        max_iter=max_iter,
+        convergence_threshold=convergence_threshold,
+        profit_function=(p_1, p_2) -> π_fun(p_1, p_2, competition_params)
     )
-    return runCalvano(env)
+    return setupCalvanoExperiment(env)
 end
+
+runCalvano(env::CalvanoEnv) = setupCalvanoExperiment(env) |> run()
+
+# Look into DistributedReinforcementLearning.jl for running grid of experiments
+# Add hook `(hook::YourHook)(::PostExperimentStage, agent, env)` to save profit results!
+
+function runCalvano(
+        α::Float64,
+        β::Float64,
+        δ::Float64,
+        price_options::Base.AbstractVecOrTuple{Float64},
+        competition_params::CompetitionParameters;
+        max_iter::Int = Int(1e9),
+        convergence_threshold::Int = Int(1e5),
+    )
+    experiment = setupCalvanoExperiment(
+        α,
+        β,
+        δ,
+        price_options,
+        competition_params,
+        max_iter=max_iter,
+        convergence_threshold=convergence_threshold,
+    )
+    return run(experiment)
+end
+
+
