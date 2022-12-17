b366be3ca7fc39475c428709ccd4843154fd962f
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-12-17 19:43:00 +0100

    Refactor convergence metadata into struct

--- src/Calvano2020/hooks.jl
+++ src/Calvano2020/hooks.jl
@@ -7,14 +7,18 @@ function _convergence_check(q_table::Matrix{Float32}, convergence_table::SubArra
     return best_action, is_converged
 end
 
+struct ConvergenceMeta
+    convergence_duration::UInt16
+    convergence_metric::UInt16
+    iterations_until_convergence::UInt16
+end
+
 struct ConvergenceCheck <: AbstractHook
     n_state_space::UInt16
     n_players::UInt8
     approximator_table__state_argmax::Matrix{UInt8}
     # Number of steps where no change has happened to argmax
-    convergence_duration::Vector{UInt16}
-    convergence_metric::Vector{UInt16}
-    iterations_until_convergence::Vector{UInt16}
+    convergence_meta_tuple::Vector[ConvergenceMeta])
 
     function ConvergenceCheck(
         n_state_space::Int,
@@ -24,40 +28,56 @@ struct ConvergenceCheck <: AbstractHook
             convert(UInt16, n_state_space),
             convert(UInt8, n_players),
             zeros(UInt8, n_players, n_state_space),
-            zeros(UInt16, n_players),
-            zeros(UInt16, n_players),
-            zeros(UInt16, n_players),
+            [ConvergenceMeta(0,0,0), ConvergenceMeta(0,0,0)]
     )
     end
 end
 
-function (h::ConvergenceCheck)(::PostActStage, policy, env)
+function update_convergence_meta(
+    c_meta::ConvergenceMeta,
+    q_table::Matrix{Float32},
+    convergence_table::SubArray{UInt8},
+    state::Int
+    )
+    # Increment duration whenever argmax action is stable (convergence criteria)
+    # Increment convergence metric (e.g. convergence not reached)
+    # Keep track of number of iterations it takes until convergence
 
+    best_action, is_converged = _convergence_check(q_table, convergence_table, state)
+
+    iterations_until_convergence = c_meta.iterations_until_convergence + 1
+
+    convergence_duration = is_converged ? c_meta.convergence_duration + 1 : 0
+    convergence_metric = is_converged ? c_meta.convergence_metric : c_meta.convergence_metric + 1
+
+    return ConvergenceMeta(convergence_duration, convergence_metric, iterations_until_convergence), is_converged
+
+end
+
+function (h::ConvergenceCheck)(::PostActStage, policy, env)
     # Convergence is defined over argmax action for each state
     # E.g. best / greedy action
     current_player_id = env.current_player_idx
     # Only update best action for the state space which was played
-    if policy.policy.name == current_player_id
-        q_table = policy.policy.policy.learner.approximator.table
-        convergence_table = @view h.approximator_table__state_argmax[current_player_id, :]
-        state = RLBase.state(env)
-        best_action, is_converged = _convergence_check(q_table, convergence_table, state)
-
-
-        # Increment duration whenever argmax action is stable (convergence criteria)
-        # Increment convergence metric (e.g. convergence not reached)
-        # and update argmax matrix
-        # Keep track of number of iterations it takes until convergence
-        h.iterations_until_convergence[current_player_id] += 1
-
-        if is_converged
-            h.convergence_duration[current_player_id] += 1
-        else
-            h.convergence_duration[current_player_id] = 0
-            h.approximator_table__state_argmax[current_player_id, state] = best_action
-            h.convergence_metric[current_player_id] += 1
-        end
+    if policy.policy.name != current_player_id
+        return
     end
+
+    q_table = policy.policy.policy.learner.approximator.table
+    convergence_table = @view h.approximator_table__state_argmax[current_player_id, :]
+    state = RLBase.state(env)
+    
+    c_meta, is_converged = update_convergence_meta(
+        h.convergence_meta_tuple[current_player_id],
+        q_table,
+        convergence_table,
+        state
+    )
+
+    # Update argmax matrix
+    is_converged && h.approximator_table__state_argmax[current_player_id, state] = best_action
+    return
+
 end
 
 function CalvanoHook(env::AbstractEnv)
