5dc106e6de4f53c5a5a2046b56de080d1d994ca1
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-12-24 14:19:50 -0600

    Add parallel code

---
+++ src/Calvano2020/parallel.jl
@@ -0,0 +1,20 @@
+function run_pmap(
+    i::Int,
+    param_set::Vector{Float64},
+    δ::Float64,
+    price_options::Vector{Float64},
+    competition_params::CompetitionParameters,
+    p_Bert_nash_equilibrium::Float64,
+    p_monop_opt::Float64
+    )
+    o_ = runCalvano(
+            param_set[i][1],
+            param_set[i][2],
+            δ,
+            price_options,
+            competition_params,
+            p_Bert_nash_equilibrium,
+            p_monop_opt,
+        )
+    return economic_summary(o_)
+end
