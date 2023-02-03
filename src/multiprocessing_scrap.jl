2691a216062fa2d018d878c5a0223ef3955015b1
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-02-03 11:43:47 +0100

    wip code

--- src/multiprocessing_scrap.jl
+++ src/multiprocessing_scrap.jl
@@ -1,59 +1,67 @@
 using Distributed
-# using ParallelDataTransfer
+using AlgorithmicCompetition: AlgorithmicCompetition, CalvanoHyperParameters, CalvanoEnv, Experiment
+using ReinforcementLearning
 
-_procs = addprocs(
-    4,
-    topology = :master_worker,
-    exeflags = ["--threads=1", "--project=$(Base.active_project())"],
-)
+multiproc = false
 
-@everywhere begin
-    using Pkg
-    Pkg.instantiate()
-    using AlgorithmicCompetition
+if multiproc
+    using ParallelDataTransfer
+
+    _procs = addprocs(1,
+        topology=:master_worker,
+        exeflags=["--threads=1", "--project=$(Base.active_project())"]
+        )
+
+    @everywhere begin
+        using Pkg; Pkg.instantiate()
+        using AlgorithmicCompetition: AlgorithmicCompetition, CalvanoHyperParameters, CalvanoEnv
+    end
 end
 
-const α = 0.125
-const β = 1e-5
-const δ = 0.95
-const ξ = 0.1
-const δ = 0.95
-const n_prices = 15
-const price_index = 1:n_prices
-
-const competition_params = AlgorithmicCompetition.CompetitionParameters(
-    μ = 0.25,
-    a_0 = 0,
-    a = [2, 2],
-    c = [1, 1],
-    n_firms = 2,
-)
-
-model_monop, p_monop = AlgorithmicCompetition.solve_monopolist(competition_params)
-
-const p_Bert_nash_equilibrium =
-    AlgorithmicCompetition.solve_bertrand(competition_params)[2][1]
-const p_monop_opt = AlgorithmicCompetition.solve_monopolist(competition_params)[2][1]
-
-# p_monop defined above
-p_range_pad = ξ * (p_monop_opt - p_Bert_nash_equilibrium)
-const price_options = [range(p_Bert_nash_equilibrium, p_monop_opt, n_prices)...]
-
-
-n_increments = 2 # for full sim, use 100
+n_increments = 100
+max_iter = Int(1e6) # Should be 1e9
 α_ = range(0.025, 0.25, n_increments)
-# TODO: Fix this parameterization based on Calvano pg. 12
-β_ = range(1.25e-8, 2e-5, n_increments)
+β_ = range(1.25e-8, 2e-5, n_increments) 
+δ = 0.95
 
-const param_set = [
-    (α, β, δ, price_options, competition_params, p_Bert_nash_equilibrium, p_monop_opt)
-    for α in α_ for β in β_
-]
+alpha_beta = [(α, β) for α in α_ for α_ in β_]
 
-# ν function from Calvano pg. 12
-ν(β, k, m, n) = (m - 1)^n / ((m^(k * n * (n + 1))) * (1 - exp(-β * (n + 1))))
-ν(β, 1, 15, 2) # n = n_firms, m = n_prices, k = memory(?)
-ν(1.25e-8, 1, 15, 2) # Value for ≈ 450
+@chain alpha_beta begin
+    CalvanoHyperParameters.(alpha_beta[1], alpha_beta[2], (δ,), (max_iter,))
+    CalvanoEnv.()
+    Experiment.()
+end
+
+params = CalvanoHyperParameters.(α_, β_, (δ,), (max_iter,))
+env = CalvanoEnv.(params)
+experiment = Experiment.(env)
+
+CalvanoHyperParameters.(α_, β_, (δ,), (max_iter,)) |> CalvanoEnv.() |> Experiment.()
+
+if multiproc
+    # Transfer Data to Workers
+    sendto(workers(), params=params
+        )
+
+    pmap(AlgorithmicCompetition.run, param_set)
+else
+    Base.run.(experiment)
+end
+
+# const α = 0.125
+# const β = 1e-5
+# const δ = 0.95
+# const ξ = 0.1
+# const δ = 0.95
+# const n_prices = 15
+# const price_index = 1:n_prices
+
+# # ν function from Calvano pg. 12
+# ν(β, k, m, n) = (m - 1)^n / ((m^(k * n * (n + 1))) * (1 - exp(- β * (n+1))))
+# ν(β, 1, 15, 2) # n = n_firms, m = n_prices, k = memory(?)
+# ν(1.25e-8, 1, 15, 2) # Value for ≈ 450
+
+# n_increments = 2 #s for full sim, use 100
 
 # Transfer Data to Workers
 # sendto(workers(), δ=δ,
@@ -79,7 +87,6 @@ const param_set = [
 # e_out = pmap_experiments()
 # rmprocs(_procs)
 
-a = pmap(AlgorithmicCompetition.run_pmap, _procs, param_set)
 
 # TODOs
 # - Add hook to save profit results
