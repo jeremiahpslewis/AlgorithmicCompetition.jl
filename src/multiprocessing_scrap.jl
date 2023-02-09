045172ffeae05cd0c1883a6eee68d3d887caf397
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-02-09 15:37:02 +0100

    Full scrap setup

--- src/multiprocessing_scrap.jl
+++ src/multiprocessing_scrap.jl
@@ -35,8 +35,7 @@ competition_params = CompetitionParameters(0.25, 0, [2, 2], [1, 1])
 
 competition_solution = CompetitionSolution(competition_params)
 
-# n_increments = 100
-n_increments = 10
+n_increments = 100
 max_iter = Int(1e6) # Should be 1e9
 α_ = range(0.025, 0.25, n_increments)
 β_ = range(1.25e-8, 2e-5, n_increments)
@@ -59,7 +58,7 @@ avg_profit_result = [ex.avg_profit[1] for ex in exp_list]
 
 df = DataFrame(α = α_result, β = β_result, π_bar = avg_profit_result)
 
-CSV.write(df, "simulation_results.csv")
+CSV.write("simulation_results.csv", df)
 plt_ = @chain df @combine(heatmap(:α, :β, :π_bar))
 
-save(plt_[1, :α_β_π_bar_heatmap], "test.svg")
+save("test.png", plt_[1, :α_β_π_bar_heatmap])
