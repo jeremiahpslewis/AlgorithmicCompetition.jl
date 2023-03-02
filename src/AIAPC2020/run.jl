e9b149510191cfa82c19208629631cbd113c6b0d
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-03-02 10:47:08 +0100

    Drop experiment started log

--- src/AIAPC2020/run.jl
+++ src/AIAPC2020/run.jl
@@ -1,37 +1,28 @@
 using ReinforcementLearning
 using Distributed
 
-function Experiment(env::AIAPCEnv; stop_on_convergence = true)
+function Experiment(env::AIAPCEnv; stop_on_convergence=true)
     ReinforcementLearning.Experiment(
-        policy = AIAPCPolicy(env),
-        env = SequentialEnv(env),
-        stop_condition = AIAPCStop(env; stop_on_convergence = stop_on_convergence),
-        hook = AIAPCHook(env),
+        policy=AIAPCPolicy(env),
+        env=SequentialEnv(env),
+        stop_condition=AIAPCStop(env; stop_on_convergence=stop_on_convergence),
+        hook=AIAPCHook(env),
     )
 end
 
 function run(experiments::Vector{ReinforcementLearningCore.Experiment})
-    sendto(workers(), experiments = experiments)
+    sendto(workers(), experiments=experiments)
     status = pmap(1:length(experiments)) do i
-        # try
-        Base.run(experiments[i])
-        # catch e
-        #     @warn "failed to process $(i)"
-        #     false # failure
-        # end
+        Base.run(experiments[i]; describe=false)
     end
 end
 
-function run(hyperparameter_vect::Vector{AIAPCHyperParameters}; stop_on_convergence = true)
-    pmap(x -> run(x; stop_on_convergence = stop_on_convergence), hyperparameter_vect)
-end
-
-function run(hyperparameters::AIAPCHyperParameters; stop_on_convergence = true)
+function run(hyperparameters::AIAPCHyperParameters; stop_on_convergence=true)
     env = AIAPCEnv(hyperparameters)
-    experiment = Experiment(env; stop_on_convergence = stop_on_convergence)
-    return Base.run(experiment)
+    experiment = Experiment(env; stop_on_convergence=stop_on_convergence)
+    return Base.run(experiment; describe=false)
 end
 
-function run_and_extract(hyperparameters::AIAPCHyperParameters; stop_on_convergence = true)
-    economic_summary(run(hyperparameters; stop_on_convergence = stop_on_convergence))
+function run_and_extract(hyperparameters::AIAPCHyperParameters; stop_on_convergence=true)
+    economic_summary(run(hyperparameters; stop_on_convergence=stop_on_convergence))
 end
