ae26334608e4adb48571b9e0e245eb634e8cc6c5
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-03-04 00:03:34 +0100

    Fix import order

--- src/ReinforcementLearningPatches/ReinforcementLearningPatches.jl
+++ src/ReinforcementLearningPatches/ReinforcementLearningPatches.jl
@@ -1,9 +1,9 @@
 # Code borrowed from RL.jl, only needed during RL.jl refactor, to be dropped once v0.11 is released.
 include("stop_conditions.jl")
-include("multi_agent_hook.jl")
 include("policy.jl")
 include("named_policy.jl")
 include("multi_agent.jl")
+include("multi_agent_hook.jl")
 include("linear_approximator.jl")
 include("td_learner.jl")
 include("reinforcement_learning_patch.jl")
