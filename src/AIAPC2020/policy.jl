d50a7df81d18d399882402f37222204c45e9718f
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-03-03 23:42:45 +0100

    Bug fix

--- src/AIAPC2020/policy.jl
+++ src/AIAPC2020/policy.jl
@@ -7,30 +7,28 @@ end
 
 AIAPCPolicy(env::AIAPCEnv) = MultiAgentManager(
     (
-        Agent(
-            policy = NamedPolicy(
-                p, QBasedPolicy(;
-                    learner = TDLearner(;
-                        # LinearQApproximator with specified init matrix
-                        approximator = LinearApproximator( # Renamed LinearApproximator on master branch
-                            InitMatrix(env.n_prices, env.n_state_space),
-                            Descent(env.α),
-                        ),
-                        # For param info: https://github.com/JuliaReinforcementLearning/ReinforcementLearning.jl/blob/f97747923c6d7bbc5576f81664ed7b05a2ab8f1e/src/ReinforcementLearningZoo/src/algorithms/tabular/td_learner.jl#L15
-                        method = :SARS,
-                        γ = env.δ,
-                        n = 0,
+        p => Agent(
+            QBasedPolicy(;
+                learner = TDLearner(;
+                    # LinearQApproximator with specified init matrix
+                    approximator = LinearApproximator( # Renamed LinearApproximator on master branch
+                        InitMatrix(env.n_prices, env.n_state_space),
+                        Descent(env.α),
                     ),
-                    explorer = AIAPCEpsilonGreedyExplorer(Float32(1e-5)),
+                    # For param info: https://github.com/JuliaReinforcementLearning/ReinforcementLearning.jl/blob/f97747923c6d7bbc5576f81664ed7b05a2ab8f1e/src/ReinforcementLearningZoo/src/algorithms/tabular/td_learner.jl#L15
+                    method = :SARS,
+                    γ = env.δ,
+                    n = 0,
                 ),
+                explorer = AIAPCEpsilonGreedyExplorer(Float32(1e-5)),
             ),
-            trajectory = Trajectory(CircularArraySARTTraces(;
-            capacity=3,
-            state=Int16 => (2, 3),
-            action= Union{Int8,NoOp} => (2,),
-            reward=Float32 => (),
-            terminal=Bool => ()
-            ), DummySampler()),
+        Trajectory(CircularArraySARTTraces(;
+        capacity=3,
+        state=Int16 => (2, 3),
+        action= Union{Int8,NoOp} => (2,),
+        reward=Float32 => (),
+        terminal=Bool => ()
+        ), DummySampler()),
         ) for p in players(env)
     )...,
 )
