0bade8d5bf13abae93b442453474d8e4a6665145
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-03-04 17:12:00 +0100

    Experiment syntax

--- src/AIAPC2020/run.jl
+++ src/AIAPC2020/run.jl
@@ -3,25 +3,25 @@ using ReinforcementLearningEnvironments
 using Distributed
 
 function Experiment(env::AIAPCEnv; stop_on_convergence = true)
-    Experiment(
-        policy = AIAPCPolicy(env),
-        env = SequentialEnv(env),
-        stop_condition = AIAPCStop(env; stop_on_convergence = stop_on_convergence),
-        hook = AIAPCHook(env),
+    RLCore.Experiment(
+        AIAPCPolicy(env),
+        SequentialEnv(env),
+        AIAPCStop(env; stop_on_convergence = stop_on_convergence),
+        AIAPCHook(env),
     )
 end
 
 function run(experiments::Vector{ReinforcementLearningCore.Experiment})
     sendto(workers(), experiments = experiments)
     status = pmap(1:length(experiments)) do i
-        Base.run(experiments[i]; describe = false)
+        Base.run(experiments[i])
     end
 end
 
 function run(hyperparameters::AIAPCHyperParameters; stop_on_convergence = true)
     env = AIAPCEnv(hyperparameters)
     experiment = Experiment(env; stop_on_convergence = stop_on_convergence)
-    return Base.run(experiment; describe = false)
+    return Base.run(experiment)
 end
 
 function run_and_extract(hyperparameters::AIAPCHyperParameters; stop_on_convergence = true)
