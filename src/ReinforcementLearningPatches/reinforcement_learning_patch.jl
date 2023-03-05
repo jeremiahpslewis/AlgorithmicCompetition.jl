7ac00655aec66429d3aa31fb8c09e61fb3c0fa90
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-03-05 10:18:29 +0100

    Patch QBasedPolicy

--- src/ReinforcementLearningPatches/reinforcement_learning_patch.jl
+++ src/ReinforcementLearningPatches/reinforcement_learning_patch.jl
@@ -123,3 +123,6 @@ function run_fixed(x::Experiment)
     Base.run(x.policy, x.env, x.stop_condition, x.hook)
     x
 end
+
+# Patch for QBasedPolicy, not sure why NamedTuple dispatch is not working
+RLBase.optimise!(p::QBasedPolicy, x::CircularArraySARTTraces) = optimise!(p.learner, x)
