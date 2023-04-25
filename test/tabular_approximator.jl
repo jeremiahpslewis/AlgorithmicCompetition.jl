986eae320042f7b9b869cd732cc483e7917b5535
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-04-25 16:42:03 +0200

    TabularApprox

--- test/tabular_approximator.jl
+++ test/tabular_approximator.jl
@@ -1,10 +1,13 @@
 using Flux
 using AlgorithmicCompetition: TabularApproximator, TabularVApproximator, TabularQApproximator, TDLearner
+using ReinforcementLearningBase
 using Test
 
 @testset "TabularApproximator" begin
-    table_q = zeros(Float32, 10, 200)
-    table_v = zeros(Float32, 200)
+    n_state = 200
+    n_action = 10
+    table_q = zeros(Float32, n_action, n_state)
+
     optimizer_ = Descent(0.125)
 
     tabular_q_approx = TabularApproximator(table_q, Descent(0.125))
@@ -12,14 +15,21 @@ using Test
     @test tabular_q_approx.optimizer isa Descent
     @test tabular_q_approx(10) isa SubArray
     @test tabular_q_approx(10, 1) isa Float32
+    RLBase.optimise!(tabular_q_approx, (10,1) => 1.0)
+    @test tabular_q_approx.table[1,10] == -0.125f0
+    RLBase.optimise!(tabular_q_approx, 11 => fill(1.0, n_action))
+    @test tabular_q_approx.table[1,11] == -0.125f0
 
+    table_v = zeros(Float32, n_state)
     tabular_v_approx = TabularApproximator(table_v, Descent(0.125))
     @test tabular_v_approx.table == table_v
     @test tabular_v_approx.optimizer isa Descent
     @test tabular_v_approx(10) isa Float32
+    RLBase.optimise!(tabular_v_approx, 10 => 1.0)
+    @test tabular_v_approx.table[10] == -0.125f0
 
-    @test TabularVApproximator(; n_state=10).table == zeros(Float32, 10)
-    @test TabularQApproximator(; n_state=10, n_action=2).table == zeros(Float32, 2, 10)
+    @test TabularVApproximator(; n_state=n_state, init=Float32(0.0)).table == zeros(Float32, n_state)
+    @test TabularQApproximator(; n_state=n_state, n_action=n_action, init=Float32(0.0)).table == zeros(Float32, n_action, n_state)
 end
 
 
