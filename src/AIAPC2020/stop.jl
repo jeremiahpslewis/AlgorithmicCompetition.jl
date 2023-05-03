bb7d90500ca8ff92c995f4c9edbdf0190a60af24
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-05-03 18:39:13 +0200

    Fix convergence thresh

--- src/AIAPC2020/stop.jl
+++ src/AIAPC2020/stop.jl
@@ -3,7 +3,8 @@ using ReinforcementLearningCore
 struct StopWhenConverged end
 
 function (s::StopWhenConverged)(agent, env)
-    return env.convergence_dict[:1] > env.convergence_threshold && env.convergence_dict[:2] > env.convergence_threshold
+    # false until converged, then true
+    return env.convergence_dict[:1] <= env.convergence_threshold || env.convergence_dict[:2] < env.convergence_threshold
 end
 
 function AIAPCStop(env::AIAPCEnv; stop_on_convergence = true)
