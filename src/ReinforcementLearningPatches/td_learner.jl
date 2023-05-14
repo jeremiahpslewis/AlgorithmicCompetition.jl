8e5ba08faaa0c6f7f0bab3dec64323f227721aa5
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-05-14 13:07:29 +0200

    Swap out td_learner.jl

--- src/ReinforcementLearningPatches/td_learner.jl
+++ src/ReinforcementLearningPatches/td_learner.jl
@@ -4,55 +4,56 @@ using LinearAlgebra: dot
 using Distributions: pdf
 using ReinforcementLearningBase
 using ReinforcementLearningCore
-import ReinforcementLearningCore: RLCore
 using ReinforcementLearningTrajectories: Trajectory
 
-Base.@kwdef struct TDLearner{F<:AbstractFloat,I<:Integer} <: AbstractLearner
-    approximator::TabularApproximator
+struct TDLearnerSARS{A,F<:AbstractFloat,I<:Integer} <: AbstractLearner
+    approximator::A
     γ::F = 1.0
     method::Symbol
     n::I = 0
+
+    function TDLearner(; approximator::A, γ::F, method::Symbol, n::I) where {A,F,I}
+        if method != :SARS
+            @error "unsupported method"
+        else
+            new{A,F,I}(approximator, γ, method, n)
+        end
+    end
 end
 
-# RLCore.estimate_reward(L::TDLearner, env::E) where {E<:AbstractEnv} = _get_qapproximator(L.approximator.table, RLBase.state(env))
-# RLCore.estimate_reward(L::TDLearner, s) = RLCore.estimate_reward(L.approximator, s)
-# RLCore.estimate_reward(L::TDLearner, s, a) = RLCore.estimate_reward(L.approximator, s, a)
+RLCore.estimate_reward(L::TDLearnerSARS{A,F,I}, env::E) where {A,F,I,E<:AbstractEnv} = RLCore.estimate_reward(L.approximator, state(env))
+RLCore.estimate_reward(L::TDLearnerSARS{A,F,I}, s::I1) where {A,F,I1<:Integer} = RLCore.estimate_reward(L.approximator, s)
+RLCore.estimate_reward(L::TDLearnerSARS{A,F,I}, s::I1, a::I2) where {A,F,I,I1<:Integer,I2<:Integer} = RLCore.estimate_reward(L.approximator, s, a)
 
-function RLBase.optimise!(L::TDLearner, t)
-    _optimise!(L, t)
-    return
-end
-
-# NOTE: Indexing is hard-coded due to RLTrajectories type instability
-function _optimise!(L::TDLearner, t::Tr) where {Tr<:Traces}
+function extract_sar(t::Tr) where {Tr<:Traces}
+    # TODO: Delete this when RLTrajectories.jl is fixed
+    # Hard coded to deal with index type instability in RLTrajectories.jl
     S = t.traces[1][:state][1]
     A = t.traces[2][:action][1]
     R = t.traces[3][1]
-
-    _optimise!(L, L.approximator, L.n, L.γ, S, A, R)
-    return
 end
 
-function _optimise!(Q::TDLearner, approximator::TabularApproximator{2,Rd,O}, n, γ, S, A, R) where {Rd,O}
+function RLBase.optimise!(L::TDLearnerSARS{A,F,I}, t::Tr) where {A,F,I,Tr<:Traces}
+    # S, A, R, T = (t[x][1] for x in SART)
+    S, A, R = extract_sar(t) # Remove this when the above line works without a performance hit
+    n, γ, Q = L.n, L.γ, L.approximator
     G = 0.0
     for i = 1:min(n + 1, length(R))
         G = R + γ * G
         s, a = S[end-i], A[end-i]
-        e = RLCore.estimate_reward(approximator, s, a) - G
-        RLBase.optimise!(Q.approximator, (s, a) => e)
+        RLBase.optimise!(Q, (s, a) => RLCore.estimate_reward(Q, s, a) - G)
     end
-    return
 end
 
-function RLBase.priority(L::TDLearner, transition::Tuple)
-    if L.method == :SARS
+function RLBase.priority(L::TDLearnerSARS{A,F,I}, transition::Tuple{T}) where {A,F,I,T}
         s, a, r, d, s′ = transition
         γ, Q = L.γ, L.approximator
-        Δ = d ? (r - RLCore.estimate_reward(Q, s, a)) : (r + γ^(L.n + 1) * maximum(RLCore.estimate_reward(Q, s′)) - RLCore.estimate_reward(Q, s, a))
+        if d
+            Δ = (r - RLCore.estimate_reward(Q, s, a))
+        else
+            Δ = (r + γ * RLCore.estimate_reward(Q, s′) - RLCore.estimate_reward(Q, s, a))
+        end
         Δ = [Δ]  # must be broadcastable in Flux.Optimise
         Flux.Optimise.apply!(Q.optimizer, (s, a), Δ)
         abs(Δ[])
-    else
-        @error "unsupported method"
-    end
 end
