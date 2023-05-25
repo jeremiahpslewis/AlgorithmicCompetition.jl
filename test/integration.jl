605dc9356a903607b73c2b30207f29b8d947be3d
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-05-25 13:56:40 +0200

    Bug fixes

--- test/integration.jl
+++ test/integration.jl
@@ -454,12 +454,12 @@ end
         ϵ_stable = 0,
         decay_steps = Int(round(1 / 1e-5)),
     )
-    @test RLCore.get_ϵ(explorer, 1e5) ≈ 0.36787944117144233
-    @test_broken RLCore.get_ϵ(explorer, 1e5) ≈ 0.14 # Percentage cited in AIAPC paper
+    @test RLCore.get_ϵ(explorer, 1e5) ≈ 0.36787944117144233 # Percentage according to formula
+    @test_broken RLCore.get_ϵ(explorer, 1e5) ≈ 0.1353352832366127 # Percentage cited in AIAPC paper (2x step count)
 
     explorer = AIAPCEpsilonGreedyExplorer(Float64(1e-5))
-    @test get_ϵ(explorer, 1e5) ≈ 0.36787944117144233
-    @test_broken get_ϵ(explorer, 1e5) ≈ 0.14 # Percentage cited in AIAPC paper
+    @test_broken get_ϵ(explorer, 1e5) ≈ 0.36787944117144233 # Percentage according to formula
+    @test get_ϵ(explorer, 1e5) ≈ 0.1353352832366127 # Percentage cited in AIAPC paper (2x step count)
 end
 
 @testset "ConvergenceCheck" begin
