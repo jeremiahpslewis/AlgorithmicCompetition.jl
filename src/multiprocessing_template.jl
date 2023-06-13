6c23846c328fcb68a3ab311ea1a54df975bfb50b
Jeremiah Lewis <--get>
2023-06-13 17:57:47 +0200

    fixes

--- src/multiprocessing_template.jl
+++ src/multiprocessing_template.jl
@@ -39,11 +39,24 @@ using CairoMakie
 using Chain
 using DataFrameMacros
 using AlgebraOfGraphics
+using AlgorithmicCompetition: AIAPCHyperParameters, AIAPCEnv, CompetitionParameters, CompetitionSolution, profit_gain
+
+competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
+competition_solution = CompetitionSolution(competition_params)
+
+env =
+    AIAPCHyperParameters(
+        Float64(0.1),
+        Float64(1e-4),
+        0.95,
+        Int(1e7),
+        competition_solution,
+    ) |> AIAPCEnv
 
 df_summary = @chain df begin
     @groupby(:α, :β)
     @combine(
-        :Δ_π_bar = mean(:π_bar),
+        :Δ_π_bar = profit_gain(mean(:π_bar), env),
         :iterations_until_convergence = mean(:iterations_until_convergence)
     )
 end
@@ -55,7 +68,7 @@ end
 draw(plt1)
 
 plt2 = @chain df_summary begin
-    data(_) * mapping(:β, :α, :iterations_until_convergence => log10) * visual(Heatmap)
+    data(_) * mapping(:β, :α, :iterations_until_convergence) * visual(Heatmap)
 end
 
 draw(plt2)
