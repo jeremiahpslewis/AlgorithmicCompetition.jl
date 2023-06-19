c22b46a8f4c9af594bbf803daea4a4f3ac13e535
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-06-19 15:00:38 +0200

    rearrange tests

--- test/integration.jl
+++ test/integration.jl
@@ -237,108 +237,6 @@ end
     @test cache_2.action != cache_3.action
 end
 
-@testset "run full AIAPC simulation (with full convergence threshold)" begin
-    α = Float64(0.075)
-    β = Float64(0.25e-5)
-    δ = 0.95
-    ξ = 0.1
-    δ = 0.95
-    n_prices = 15
-    max_iter = Int(1e9)
-    price_index = 1:n_prices
-
-    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
-
-    competition_solution = CompetitionSolution(competition_params)
-
-    hyperparameters = AIAPCHyperParameters(α, β, δ, max_iter, competition_solution)
-
-    profit_gain_max = 0
-    i = 0
-    while (profit_gain_max <= 0.82) && (i < 5)
-        i += 1
-        c_out = run(hyperparameters; stop_on_convergence = true)
-        profit_gain_max =
-            maximum(profit_gain(economic_summary(c_out).convergence_profit, c_out.env))
-    end
-    @test profit_gain_max > 0.82
-
-end
-
-@testset "run full AIAPC simulation" begin
-    α = Float64(0.125)
-    β = Float64(1e-5)
-    δ = 0.95
-    ξ = 0.1
-    δ = 0.95
-    n_prices = 15
-    max_iter = Int(1e5)
-    price_index = 1:n_prices
-
-    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
-
-    competition_solution = CompetitionSolution(competition_params)
-
-    hyperparameters = AIAPCHyperParameters(
-        α,
-        β,
-        δ,
-        max_iter,
-        competition_solution;
-        convergence_threshold = 10,
-    )
-
-    c_out = run(hyperparameters; stop_on_convergence = true)
-    # ensure that the policy is updated by the learner
-    @test sum(c_out.policy[Symbol(1)].policy.learner.approximator.table; dims = 2) != 0
-    state_sum = sum(c_out.policy[Symbol(1)].policy.learner.approximator.table; dims = 1)
-    @test !all(y -> y == state_sum[1], state_sum)
-    @test length(reward(c_out.env)) == 2
-    @test length(reward(c_out.env, 1)) == 1
-
-    c_out.env.is_done[1] = false
-    @test reward(c_out.env) == [0, 0]
-    @test reward(c_out.env, 1) != 0
-
-    @test sum(c_out.hook[Symbol(1)][2].best_response_vector == 0) == 0
-    @test c_out.hook[Symbol(1)][2].best_response_vector !=
-          c_out.hook[Symbol(2)][2].best_response_vector
-end
-
-@testset "Run a set of experiments." begin
-    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
-
-    competition_solution = CompetitionSolution(competition_params)
-
-    n_parameter_increments = 3
-    α_ = Float64.(range(0.0025, 0.25, n_parameter_increments))
-    β_ = Float64.(range(0.025, 2, n_parameter_increments)) * 1e-5
-    δ = 0.95
-    max_iter = Int(1e8)
-
-    hyperparameter_vect = [
-        AIAPCHyperParameters(
-            α,
-            β,
-            δ,
-            max_iter,
-            competition_solution;
-            convergence_threshold = 10,
-        ) for α in α_ for β in β_
-    ]
-
-    experiments = @chain hyperparameter_vect run_and_extract.(stop_on_convergence = true)
-
-    @test experiments[1] isa AIAPCSummary
-    @test all(10 < experiments[1].iterations_until_convergence[i] < max_iter for i = 1:2)
-    @test (
-        sum(experiments[1].convergence_profit .> 1) +
-        sum(experiments[1].convergence_profit .< 0)
-    ) == 0
-    @test experiments[1].convergence_profit[1] != experiments[1].convergence_profit[2]
-    @test all(experiments[1].is_converged)
-end
-
 @testset "Profit gain check" begin
     competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
     competition_solution = CompetitionSolution(competition_params)
@@ -483,68 +381,6 @@ end
     @test a[5, 10] ≈ 4.521381412143576
 end
 
-@testset "Parameter / learning checks" begin
-    α = Float64(0.125)
-    β = Float64(1e-5)
-    δ = 0.95
-    ξ = 0.1
-    δ = 0.95
-    n_prices = 15
-    max_iter = 10000
-    price_index = 1:n_prices
-
-    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
-
-    competition_solution = CompetitionSolution(competition_params)
-
-    hyperparameters = AIAPCHyperParameters(
-        α,
-        β,
-        δ,
-        max_iter,
-        competition_solution;
-        convergence_threshold = 1,
-    )
-
-
-    c_out = run(hyperparameters; stop_on_convergence = false)
-
-    # ensure that the policy is updated by the learner
-    @test sum(c_out.policy[Symbol(1)].policy.learner.approximator.table .!= 0) != 0
-    @test sum(c_out.policy[Symbol(2)].policy.learner.approximator.table .!= 0) != 0
-    @test c_out.env.is_done[1]
-    @test c_out.hook[Symbol(1)][2].iterations_until_convergence == max_iter
-    @test c_out.hook[Symbol(2)][2].iterations_until_convergence == max_iter
-
-
-    @test c_out.policy[Symbol(1)].trajectory.container[:reward][1] .!= 0
-    @test c_out.policy[Symbol(2)].trajectory.container[:reward][1] .!= 0
-
-    @test c_out.policy[Symbol(1)].policy.learner.approximator.table !=
-          c_out.policy[Symbol(2)].policy.learner.approximator.table
-    @test c_out.hook[Symbol(1)][2].best_response_vector !=
-          c_out.hook[Symbol(2)][2].best_response_vector
-
-
-    @test mean(
-        c_out.hook[Symbol(1)][1].rewards[(end-2):end] .!=
-        c_out.hook[Symbol(2)][1].rewards[(end-2):end],
-    ) >= 0.3
-
-    for i in [Symbol(1), Symbol(2)]
-        @test c_out.hook[i][2].convergence_duration >= 0
-        @test c_out.hook[i][2].is_converged
-        @test c_out.hook[i][2].convergence_threshold == 1
-        @test sum(c_out.hook[i][1].rewards .== 0) == 0
-    end
-
-    @test reward(c_out.env, 1) != 0
-    @test reward(c_out.env, 2) != 0
-    @test length(reward(c_out.env)) == 2
-    @test length(c_out.env.action_space) == 225
-    @test length(reward(c_out.env)) == 2
-end
-
 @testset "Sequential environment" begin
     α = Float64(0.125)
     β = Float64(1e-5)
@@ -574,67 +410,6 @@ end
     @test reward(env) != 0 # reward reflects outcomes of last play (which happens at player = 1, e.g. before any actions chosen)
     act!(env, (5, 5))
     @test reward(env) != [0, 0] # reward is zero as at least one player has already played (technically sequental plays)
-end
-
-@testset "No stop on Convergence stop works" begin
-    α = Float64(0.125)
-    β = Float64(1e-5)
-    δ = 0.95
-    ξ = 0.1
-    δ = 0.95
-    n_prices = 15
-    max_iter = Int(1e6)
-    price_index = 1:n_prices
-
-    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
-    competition_solution = CompetitionSolution(competition_params)
-
-    hyperparameters = AIAPCHyperParameters(
-        α,
-        β,
-        δ,
-        max_iter,
-        competition_solution;
-        convergence_threshold = 10,
-    )
-    c_out = run(hyperparameters; stop_on_convergence = false)
-    @test get_ϵ(c_out.policy[Symbol(1)].policy.explorer) < 1e-4
-    @test get_ϵ(c_out.policy[Symbol(2)].policy.explorer) < 1e-4
-end
-
-@testset "Convergence stop works" begin
-    α = Float64(0.125)
-    β = Float64(1e-5)
-    δ = 0.95
-    ξ = 0.1
-    δ = 0.95
-    n_prices = 15
-    max_iter = Int(1e7)
-    price_index = 1:n_prices
-
-    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
-    competition_solution = CompetitionSolution(competition_params)
-
-    hyperparameters = AIAPCHyperParameters(
-        α,
-        β,
-        δ,
-        max_iter,
-        competition_solution;
-        convergence_threshold = 5,
-    )
-    c_out = run(hyperparameters; stop_on_convergence = true)
-    @test 0.98 < get_ϵ(c_out.policy[Symbol(1)].policy.explorer) < 1
-    @test 0.98 < get_ϵ(c_out.policy[Symbol(2)].policy.explorer) < 1
-
-    @test RLCore.check_stop(c_out.stop_condition, 1, c_out.env) == true
-    @test RLCore.check_stop(c_out.stop_condition.stop_conditions[1], 1, c_out.env) == false
-    @test RLCore.check_stop(c_out.stop_condition.stop_conditions[2], 1, c_out.env) == true
-
-    @test c_out.hook[Symbol(1)][2].convergence_duration >= 5
-    @test c_out.hook[Symbol(2)][2].convergence_duration >= 5
-    @test (c_out.hook[Symbol(2)][2].convergence_duration == 5) ||
-          (c_out.hook[Symbol(1)][2].convergence_duration == 5)
 end
 
 @testset "EpsilonGreedy" begin
@@ -704,4 +479,229 @@ end
     )
 
     rmprocs(_procs)
+end
+
+@testset "run full AIAPC simulation (with full convergence threshold)" begin
+    α = Float64(0.075)
+    β = Float64(0.25e-5)
+    δ = 0.95
+    ξ = 0.1
+    δ = 0.95
+    n_prices = 15
+    max_iter = Int(1e9)
+    price_index = 1:n_prices
+
+    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
+
+    competition_solution = CompetitionSolution(competition_params)
+
+    hyperparameters = AIAPCHyperParameters(α, β, δ, max_iter, competition_solution)
+
+    profit_gain_max = 0
+    i = 0
+    while (profit_gain_max <= 0.82) && (i < 5)
+        i += 1
+        c_out = run(hyperparameters; stop_on_convergence = true)
+        profit_gain_max =
+            maximum(profit_gain(economic_summary(c_out).convergence_profit, c_out.env))
+    end
+    @test profit_gain_max > 0.82
+
+end
+
+@testset "run full AIAPC simulation" begin
+    α = Float64(0.125)
+    β = Float64(1e-5)
+    δ = 0.95
+    ξ = 0.1
+    δ = 0.95
+    n_prices = 15
+    max_iter = Int(1e5)
+    price_index = 1:n_prices
+
+    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
+
+    competition_solution = CompetitionSolution(competition_params)
+
+    hyperparameters = AIAPCHyperParameters(
+        α,
+        β,
+        δ,
+        max_iter,
+        competition_solution;
+        convergence_threshold = 10,
+    )
+
+    c_out = run(hyperparameters; stop_on_convergence = true)
+    # ensure that the policy is updated by the learner
+    @test sum(c_out.policy[Symbol(1)].policy.learner.approximator.table; dims = 2) != 0
+    state_sum = sum(c_out.policy[Symbol(1)].policy.learner.approximator.table; dims = 1)
+    @test !all(y -> y == state_sum[1], state_sum)
+    @test length(reward(c_out.env)) == 2
+    @test length(reward(c_out.env, 1)) == 1
+
+    c_out.env.is_done[1] = false
+    @test reward(c_out.env) == [0, 0]
+    @test reward(c_out.env, 1) != 0
+
+    @test sum(c_out.hook[Symbol(1)][2].best_response_vector == 0) == 0
+    @test c_out.hook[Symbol(1)][2].best_response_vector !=
+          c_out.hook[Symbol(2)][2].best_response_vector
+end
+
+@testset "Run a set of experiments." begin
+    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
+
+    competition_solution = CompetitionSolution(competition_params)
+
+    n_parameter_increments = 3
+    α_ = Float64.(range(0.0025, 0.25, n_parameter_increments))
+    β_ = Float64.(range(0.025, 2, n_parameter_increments)) * 1e-5
+    δ = 0.95
+    max_iter = Int(1e8)
+
+    hyperparameter_vect = [
+        AIAPCHyperParameters(
+            α,
+            β,
+            δ,
+            max_iter,
+            competition_solution;
+            convergence_threshold = 10,
+        ) for α in α_ for β in β_
+    ]
+
+    experiments = @chain hyperparameter_vect run_and_extract.(stop_on_convergence = true)
+
+    @test experiments[1] isa AIAPCSummary
+    @test all(10 < experiments[1].iterations_until_convergence[i] < max_iter for i = 1:2)
+    @test (
+        sum(experiments[1].convergence_profit .> 1) +
+        sum(experiments[1].convergence_profit .< 0)
+    ) == 0
+    @test experiments[1].convergence_profit[1] != experiments[1].convergence_profit[2]
+    @test all(experiments[1].is_converged)
+end
+
+@testset "Parameter / learning checks" begin
+    α = Float64(0.125)
+    β = Float64(1e-5)
+    δ = 0.95
+    ξ = 0.1
+    δ = 0.95
+    n_prices = 15
+    max_iter = 10000
+    price_index = 1:n_prices
+
+    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
+
+    competition_solution = CompetitionSolution(competition_params)
+
+    hyperparameters = AIAPCHyperParameters(
+        α,
+        β,
+        δ,
+        max_iter,
+        competition_solution;
+        convergence_threshold = 1,
+    )
+
+
+    c_out = run(hyperparameters; stop_on_convergence = false)
+
+    # ensure that the policy is updated by the learner
+    @test sum(c_out.policy[Symbol(1)].policy.learner.approximator.table .!= 0) != 0
+    @test sum(c_out.policy[Symbol(2)].policy.learner.approximator.table .!= 0) != 0
+    @test c_out.env.is_done[1]
+    @test c_out.hook[Symbol(1)][2].iterations_until_convergence == max_iter
+    @test c_out.hook[Symbol(2)][2].iterations_until_convergence == max_iter
+
+
+    @test c_out.policy[Symbol(1)].trajectory.container[:reward][1] .!= 0
+    @test c_out.policy[Symbol(2)].trajectory.container[:reward][1] .!= 0
+
+    @test c_out.policy[Symbol(1)].policy.learner.approximator.table !=
+          c_out.policy[Symbol(2)].policy.learner.approximator.table
+    @test c_out.hook[Symbol(1)][2].best_response_vector !=
+          c_out.hook[Symbol(2)][2].best_response_vector
+
+
+    @test mean(
+        c_out.hook[Symbol(1)][1].rewards[(end-2):end] .!=
+        c_out.hook[Symbol(2)][1].rewards[(end-2):end],
+    ) >= 0.3
+
+    for i in [Symbol(1), Symbol(2)]
+        @test c_out.hook[i][2].convergence_duration >= 0
+        @test c_out.hook[i][2].is_converged
+        @test c_out.hook[i][2].convergence_threshold == 1
+        @test sum(c_out.hook[i][1].rewards .== 0) == 0
+    end
+
+    @test reward(c_out.env, 1) != 0
+    @test reward(c_out.env, 2) != 0
+    @test length(reward(c_out.env)) == 2
+    @test length(c_out.env.action_space) == 225
+    @test length(reward(c_out.env)) == 2
+end
+
+@testset "No stop on Convergence stop works" begin
+    α = Float64(0.125)
+    β = Float64(1e-5)
+    δ = 0.95
+    ξ = 0.1
+    δ = 0.95
+    n_prices = 15
+    max_iter = Int(1e6)
+    price_index = 1:n_prices
+
+    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
+    competition_solution = CompetitionSolution(competition_params)
+
+    hyperparameters = AIAPCHyperParameters(
+        α,
+        β,
+        δ,
+        max_iter,
+        competition_solution;
+        convergence_threshold = 10,
+    )
+    c_out = run(hyperparameters; stop_on_convergence = false)
+    @test get_ϵ(c_out.policy[Symbol(1)].policy.explorer) < 1e-4
+    @test get_ϵ(c_out.policy[Symbol(2)].policy.explorer) < 1e-4
+end
+
+@testset "Convergence stop works" begin
+    α = Float64(0.125)
+    β = Float64(1e-5)
+    δ = 0.95
+    ξ = 0.1
+    δ = 0.95
+    n_prices = 15
+    max_iter = Int(1e7)
+    price_index = 1:n_prices
+
+    competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
+    competition_solution = CompetitionSolution(competition_params)
+
+    hyperparameters = AIAPCHyperParameters(
+        α,
+        β,
+        δ,
+        max_iter,
+        competition_solution;
+        convergence_threshold = 5,
+    )
+    c_out = run(hyperparameters; stop_on_convergence = true)
+    @test 0.98 < get_ϵ(c_out.policy[Symbol(1)].policy.explorer) < 1
+    @test 0.98 < get_ϵ(c_out.policy[Symbol(2)].policy.explorer) < 1
+
+    @test RLCore.check_stop(c_out.stop_condition, 1, c_out.env) == true
+    @test RLCore.check_stop(c_out.stop_condition.stop_conditions[1], 1, c_out.env) == false
+    @test RLCore.check_stop(c_out.stop_condition.stop_conditions[2], 1, c_out.env) == true
+
+    @test c_out.hook[Symbol(1)][2].convergence_duration >= 5
+    @test c_out.hook[Symbol(2)][2].convergence_duration >= 5
+    @test (c_out.hook[Symbol(2)][2].convergence_duration == 5) ||
+          (c_out.hook[Symbol(1)][2].convergence_duration == 5)
 end
