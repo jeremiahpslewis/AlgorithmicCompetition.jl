e540f9fb56972e02181d41af33fc5a3733b45f9c
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-06-21 11:45:06 +0200

    Split viz and multiproc

---
+++ src/a1_viz.jl
@@ -0,0 +1,41 @@
+using CairoMakie
+using Chain
+using DataFrameMacros
+using AlgebraOfGraphics
+using AlgorithmicCompetition:
+    AIAPCHyperParameters, AIAPCEnv, CompetitionParameters, CompetitionSolution, profit_gain
+
+file_name = "simulation_results_2023-06-21T00:01:53.187"
+df = DataFrame(CSV.File(file_name))
+    
+competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
+competition_solution = CompetitionSolution(competition_params)
+
+env =
+    AIAPCHyperParameters(
+        Float64(0.1),
+        Float64(1e-4),
+        0.95,
+        Int(1e7),
+        competition_solution,
+    ) |> AIAPCEnv
+
+df_summary = @chain df begin
+    @groupby(:α, :β)
+    @combine(
+        :Δ_π_bar = profit_gain(mean(:π_bar), env),
+        :iterations_until_convergence = mean(:iterations_until_convergence)
+    )
+end
+
+plt1 = @chain df_summary begin
+    data(_) * mapping(:β, :α, :Δ_π_bar) * visual(Heatmap)
+end
+
+draw(plt1)
+
+plt2 = @chain df_summary begin
+    data(_) * mapping(:β, :α, :iterations_until_convergence) * visual(Heatmap)
+end
+
+draw(plt2)
--- src/multiprocessing_template.jl
+++ src/multiprocessing_template.jl
@@ -21,56 +21,16 @@ _procs = addprocs(
 end
 
 @time exp_list = AlgorithmicCompetition.run_aiapc(;
-    n_parameter_iterations = 10,
+    n_parameter_iterations = 30,
     n_parameter_increments = 25,
     max_iter = Int(1e9), # TODO: increment to 1e9
     # max_alpha = 0.05,
     # max_beta = 0.5,
-    # sample_fraction = 0.05,
+    sample_fraction = 0.1,
 )
 
 rmprocs(_procs)
 
+file_name = "simulation_results_$start_timestamp.csv"
 df = AlgorithmicCompetition.extract_sim_results(exp_list)
-CSV.write("simulation_results_$start_timestamp.csv", df)
-
-df = DataFrame(CSV.File("simulation_results_$start_timestamp.csv"))
-
-using CairoMakie
-using Chain
-using DataFrameMacros
-using AlgebraOfGraphics
-using AlgorithmicCompetition:
-    AIAPCHyperParameters, AIAPCEnv, CompetitionParameters, CompetitionSolution, profit_gain
-
-competition_params = CompetitionParameters(0.25, 0, (2, 2), (1, 1))
-competition_solution = CompetitionSolution(competition_params)
-
-env =
-    AIAPCHyperParameters(
-        Float64(0.1),
-        Float64(1e-4),
-        0.95,
-        Int(1e7),
-        competition_solution,
-    ) |> AIAPCEnv
-
-df_summary = @chain df begin
-    @groupby(:α, :β)
-    @combine(
-        :Δ_π_bar = profit_gain(mean(:π_bar), env),
-        :iterations_until_convergence = mean(:iterations_until_convergence)
-    )
-end
-
-plt1 = @chain df_summary begin
-    data(_) * mapping(:β, :α, :Δ_π_bar) * visual(Heatmap)
-end
-
-draw(plt1)
-
-plt2 = @chain df_summary begin
-    data(_) * mapping(:β, :α, :iterations_until_convergence) * visual(Heatmap)
-end
-
-draw(plt2)
+CSV.write(file_name, df)
