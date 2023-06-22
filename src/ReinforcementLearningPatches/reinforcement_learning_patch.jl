100cb07698565ae950800dcb13e393424f42ae1a
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-06-22 16:00:16 +0200

    Rearrange function location

--- src/ReinforcementLearningPatches/reinforcement_learning_patch.jl
+++ src/ReinforcementLearningPatches/reinforcement_learning_patch.jl
@@ -130,3 +130,20 @@ function Base.push!(
     Base.push!(hook, stage, agent, env)
     return
 end
+
+function RLBase.plan!(
+    s::AIAPCEpsilonGreedyExplorer{<:Any,F},
+    values,
+    full_action_space,
+) where {F<:AbstractFloat}
+    # NOTE: use of legal_action_space_mask as full_action_space is a bit of a hack, won't work in other cases
+    ϵ = get_ϵ(s)
+    s.step[1] += 1
+    if rand(s.rng) < ϵ
+        return rand(s.rng, full_action_space)
+    else
+        max_vals = RLCore.find_all_max(values)[2]
+
+        return Int8(rand(s.rng, max_vals))
+    end
+end
