2c699d31dd9300a27e537ccb0cfafcb2b3390b35
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-06-22 15:38:17 +0200

    Add docstrings

--- src/AIAPC2020/env.jl
+++ src/AIAPC2020/env.jl
@@ -2,6 +2,8 @@ using ReinforcementLearningCore
 using ReinforcementLearningBase
 using StaticArrays
 
+const player_lookup = (; Symbol(1) => 1, Symbol(2) => 2)
+
 """
     AIAPCEnv(p::AIAPCHyperParameters)
 
@@ -83,19 +85,36 @@ struct AIAPCEnv <: AbstractEnv
     end
 end
 
-# TODO: add back type signature ::Tuple{Int64,Int64}
+"""
+    RLBase.act!(env::AIAPCEnv, price_tuple::Tuple{Int8,Int8})
+
+Act in the environment by setting the memory to the given price tuple and setting `is_done` to `true`.
+"""
 function RLBase.act!(env::AIAPCEnv, price_tuple::Tuple{Int8,Int8})
     # TODO: Fix support for longer memories
     env.memory .= price_tuple
     env.is_done[1] = true
 end
 
+"""
+    construct_state_space_lookup(action_space, n_prices)
+
+Construct a lookup table from action space to the state space.
+"""
 function construct_state_space_lookup(action_space, n_prices)
     @assert length(action_space) == n_prices^2
     state_space_lookup = reshape(Int16.(1:length(action_space)), n_prices, n_prices)
     return state_space_lookup
 end
 
+
+"""
+    construct_profit_array(price_options, params, n_players)
+
+Construct a 3-dimensional array which holds the profit for each player given a price pair.
+The first dimension is player 1's action, the second dimension is player 2's action, and
+the third dimension is the player index for their profit.
+"""
 function construct_profit_array(
     price_options::SVector{15,Float64},
     params::CompetitionParameters,
@@ -126,29 +145,53 @@ RLBase.legal_action_space_mask(env::AIAPCEnv, player::Symbol) = SA[Int8.(1:15)..
 
 RLBase.action_space(env::AIAPCEnv) = action_space(env, SIMULTANEOUS_PLAYER)
 
+
+"""
+    RLBase.reward(env::AIAPCEnv)
+
+Return the reward for the current state. If the episode is done, return the profit, else return `(0, 0)`.
+"""
 function RLBase.reward(env::AIAPCEnv)
     env.is_done[1] ?
     (@view env.profit_array[env.memory[1], env.memory[2], :]) :
     SA[0, 0]
 end
 
+"""
+    RLBase.reward(env::AIAPCEnv, p::Int)
+
+Return the reward for the current state for player `p` as an integer. If the episode is done, return the profit, else return `0`.
+"""
 function RLBase.reward(env::AIAPCEnv, p::Int)
     (@view env.profit_array[env.memory[1], env.memory[2], p])[1]
 end
 
+"""
+    RLBase.reward(env::AIAPCEnv, p::Int)
 
-const player_lookup = (; Symbol(1) => 1, Symbol(2) => 2)
-
+Return the reward for the current state for player `p` as a symbol. If the episode is done, return the profit, else return `0`.
+"""
 RLBase.reward(env::AIAPCEnv, p::Symbol) = reward(env, player_lookup[p])
 
 RLBase.state_space(env::AIAPCEnv, ::Observation, p) = env.state_space
 
+"""
+    RLBase.state(env::AIAPCEnv, ::Observation, p)
+
+Return the current state as an integer, mapped from the environment memory.
+"""
 function RLBase.state(env::AIAPCEnv, ::Observation, p)
     env.state_space_lookup[env.memory[1], env.memory[2]]
 end
 
+"""
+    RLBase.is_terminated(env::AIAPCEnv)
+
+Return whether the episode is done.
+"""
 RLBase.is_terminated(env::AIAPCEnv) = env.is_done[1]
-# TODO: Expand reset function to other params
+
+
 function RLBase.reset!(env::AIAPCEnv)
     env.is_done[1] = false
 end
--- src/AIAPC2020/explorer.jl
+++
@@ -1,6 +0,0 @@
-import ReinforcementLearningCore: RLCore
-import ReinforcementLearningBase: RLBase
-
-function RLCore.EpsilonGreedyExplorer(decay_steps::Int)
-    EpsilonGreedyExplorer(kind = :exp, ϵ_init = 1, ϵ_stable = 0, decay_steps = decay_steps)
-end
