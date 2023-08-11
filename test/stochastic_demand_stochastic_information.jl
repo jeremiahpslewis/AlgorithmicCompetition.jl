b271e6b572e7c439196a4fb81e960c0ecc3ebfc8
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-08-11 20:50:15 +0200

    Fix reparameterization

--- test/stochastic_demand_stochastic_information.jl
+++ test/stochastic_demand_stochastic_information.jl
@@ -1,16 +1,16 @@
 @testset "get_demand_signals" begin
     @test get_demand_signals(true, [true, false], 0.0, 1.0) == [1, 0]
-    @test get_demand_signals(true, [true, false], 1.0, -1.0) == [0, 1]
+    @test get_demand_signals(true, [true, false], 1.0, 0.0) == [0, 1]
     @test get_demand_signals(false, [true, false], 0.0, 1.0) == [0, 1]
-    @test get_demand_signals(false, [true, false], 1.0, -1.0) == [1, 0]
+    @test get_demand_signals(false, [true, false], 1.0, 0.0) == [1, 0]
     @test all(
         4500 .<
         sum(get_demand_signals(false, [true, false], 0.5, 0.0) for i = 1:10000) .<
         5500,
     )
-    @test get_demand_signals(false, [true, false], 1.0, 0.0) == [0, 0]
-    @test get_demand_signals(false, [true, true], 0.5, 0.5) == [0, 0]
-    @test get_demand_signals(true, [true, true], 0.5, 0.5) == [1, 1]
+    @test get_demand_signals(false, [true, false], 1.0, 1.0) == [0, 0]
+    @test get_demand_signals(false, [true, true], 0.5, 1.0) == [0, 0]
+    @test get_demand_signals(true, [true, true], 0.5, 1.0) == [1, 1]
 end
 
 @testset "get_demand_level" begin
