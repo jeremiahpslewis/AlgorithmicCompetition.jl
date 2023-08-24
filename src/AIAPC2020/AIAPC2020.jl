6d576018beadcad70dd1cb3cf174b439dc235171
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-08-24 16:42:27 +0200

    fix setup

--- src/AIAPC2020/AIAPC2020.jl
+++ src/AIAPC2020/AIAPC2020.jl
@@ -2,7 +2,7 @@ include("params.jl")
 include("env_helpers.jl")
 include("env.jl")
 include("hooks.jl")
-include("policy.jl") # Need to finish porting!
+include("policy.jl")
 include("stop_condition.jl")
 include("run.jl")
 include("summary.jl")
