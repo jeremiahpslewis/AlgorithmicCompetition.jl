064a9ec008cad463e638a98c04c26502221fab51
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2023-08-24 16:37:28 +0200

    Fix integration test boundaries test

--- test/aiapc_conversion_check.jl
+++ test/aiapc_conversion_check.jl
@@ -26,10 +26,10 @@ function test_key_AIAPC_points(; n_parameter_iterations = 1000)
     test_params = DataFrame(
         :α => [0.15, 0.08, 0.2, 0.15, 0.01, 0.04, 0.1, 0.25, 0.2],
         :β => [0.4, 2, 0.25, 1, 0.1, 0.2, 1.75, 0.1, 1],
-        :iter_min => [0, 0, 1.5e6, 0.5e6, 0, 1.5e6, 0.2e6, 0.5e6, 0.5e6],
-        :iter_max => [1e7, 0.7e6, 1e7, 1.1e6, 1e7, 1e7, 0.5e6, 0.8e6, 1.5e6],
-        :Δ_π_bar_min => [0.75, 0.7, 0.75, 0.75, 0.6, 0.85, 0.5, 0.65, 0.55],
-        :Δ_π_bar_max => [0.9, 0.8, 0.85, 0.9, 1, 0.95, 0.8, 0.75, 0.75],
+        :iter_min => [0, 0, 1.5e6, 0.5e6, 0, 1.5e6, 0.2e6, 1e6, 0.5e6],
+        :iter_max => [1e7, 0.7e6, 1e7, 1.1e6, 1e7, 1e7, 1e6, 1e7, 1.5e6],
+        :Δ_π_bar_min => [0.75, 0.7, 0.75, 0.75, 0.6, 0.8, 0.5, 0.5, 0.55],
+        :Δ_π_bar_max => [0.9, 0.8, 0.85, 0.9, 1, 0.95, 0.8, 0.75, 0.9],
     )
     hyperparameter_vect =
         AIAPCHyperParameters.(
@@ -63,44 +63,47 @@ function test_key_AIAPC_points(; n_parameter_iterations = 1000)
 end
 
 
-n_procs_ = 7 # up to 8 performance cores on m1 (7 workers + 1 main)
+@testset "AIAPC Conversion Check" begin
+    n_procs_ = 7 # up to 8 performance cores on m1 (7 workers + 1 main)
 
-_procs = addprocs(
-    n_procs_,
-    topology = :master_worker,
-    exeflags = ["--threads=1", "--project=$(Base.active_project())"],
-)
-
-@everywhere begin
-    using Pkg
-    Pkg.instantiate()
-    using AlgorithmicCompetition: run_and_extract
-end
-
-exp_df, exp_list = test_key_AIAPC_points(; n_parameter_iterations = 10)
-rmprocs(_procs)
-
-exp_diagostic = @chain exp_df begin
-    @transform(
-        :profit_match = :Δ_π_bar_max > :Δ_π_bar > :Δ_π_bar_min,
-        :convergence_match = :iter_max > :iterations_until_convergence > :iter_min,
-        :convergence_status =
-            :iterations_until_convergence > :iter_max ? "too slow" :
-            :iterations_until_convergence < :iter_min ? "too fast" : "ok",
-        :profit_status =
-            :Δ_π_bar > :Δ_π_bar_max ? "high" : :Δ_π_bar < :Δ_π_bar_min ? "low" : "ok"
+    _procs = addprocs(
+        n_procs_,
+        topology = :master_worker,
+        exeflags = ["--threads=1", "--project=$(Base.active_project())"],
     )
-    @select(
-        :α,
-        :β,
-        :iterations_until_convergence =
-            round(:iterations_until_convergence; digits = 0),
-        :convergence_status,
-        :Δ_π_bar,
-        :profit_status
-    )
-end
 
-exp_diagostic[1:8, :]
-# @test all(exp_diagostic[!, :profit_match])
-# @test all(exp_diagostic[!, :convergence_match])
+    @everywhere begin
+        using Pkg
+        Pkg.instantiate()
+        using AlgorithmicCompetition: run_and_extract
+    end
+
+    exp_df, exp_list = test_key_AIAPC_points(; n_parameter_iterations = 10)
+    rmprocs(_procs)
+
+    exp_diagostic = @chain exp_df begin
+        @transform(
+            :profit_match = :Δ_π_bar_max > :Δ_π_bar > :Δ_π_bar_min,
+            :convergence_match = :iter_max > :iterations_until_convergence > :iter_min,
+            :convergence_status =
+                :iterations_until_convergence > :iter_max ? "too slow" :
+                :iterations_until_convergence < :iter_min ? "too fast" : "ok",
+            :profit_status =
+                :Δ_π_bar > :Δ_π_bar_max ? "high" : :Δ_π_bar < :Δ_π_bar_min ? "low" : "ok"
+        )
+        @select(
+            :α,
+            :β,
+            :iterations_until_convergence =
+                round(:iterations_until_convergence; digits = 0),
+            :convergence_status,
+            :Δ_π_bar,
+            :profit_status
+        )
+    end
+
+
+    exp_diagostic[1:8, :]
+    @test all(exp_diagostic[!, :profit_status] .== "ok")
+    @test all(exp_diagostic[!, :convergence_status])
+end
