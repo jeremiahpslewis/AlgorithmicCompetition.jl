af90d1d86dddfdef518fd9b3450cb5605826d76e
Jeremiah Lewis <--get>
2024-03-07 14:02:46 +0100

    Remove tabular_approximator.jl and update ReinforcementLearningPatches.jl

--- src/ReinforcementLearningPatches/ReinforcementLearningPatches.jl
+++ src/ReinforcementLearningPatches/ReinforcementLearningPatches.jl
@@ -1,5 +1,4 @@
 # Code borrowed from RL.jl, only needed during RL.jl refactor, to be dropped once v0.11 is released.
-include("tabular_approximator.jl")
 include("td_learner.jl")
 include("reinforcement_learning_patch.jl")
 include("q_learning.jl")
