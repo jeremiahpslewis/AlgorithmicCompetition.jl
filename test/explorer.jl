b2db8f5324c40e7dc5b861d4e584bbea1ea45a05
Jeremiah Lewis <--get>
2024-03-21 15:20:12 +0100

    fix test syntax

--- test/explorer.jl
+++ test/explorer.jl
@@ -4,6 +4,7 @@ using ReinforcementLearningFarm
 @testset "EpsilonGreedy" begin
     # This yields a different result (same result, but at 2x step count) than in the paper for 100k steps, but the same convergece duration at α and β midpoints 850k (pg. 13)
     explorer = EpsilonSpeedyExplorer(Float64(1e-5))
-    @test RLFarm.get_ϵ(explorer, Int(1e5)) ≈ 0.36787944117144233 # Percentage according to formula and paper convergence results
-    @test_broken RLFarm.get_ϵ(explorer, Int(1e5)) ≈ 0.1353352832366127 # Percentage cited in AIAPC paper (2x step count)
+    explorer.step[] = Int(1e5)
+    @test RLFarm.get_ϵ(explorer) ≈ 0.36787944117144233 # Percentage according to formula and paper convergence results
+    @test_broken RLFarm.get_ϵ(explorer) ≈ 0.1353352832366127 # Percentage cited in AIAPC paper (2x step count)
 end
