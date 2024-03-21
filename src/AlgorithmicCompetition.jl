2b5c3e37f7e9b832bd8c2985b990db416575459a
Jeremiah Lewis <--get>
2024-03-21 16:40:41 +0100

    Remove ReinforcementLearningPatches.jl import

--- src/AlgorithmicCompetition.jl
+++ src/AlgorithmicCompetition.jl
@@ -3,7 +3,6 @@ module AlgorithmicCompetition
 import Base: @invokelatest
 
 include("common.jl")
-include("ReinforcementLearningPatches/ReinforcementLearningPatches.jl")
 include("competitive_equilibrium_solver.jl")
 include("AIAPC2020/AIAPC2020.jl")
 include("DDDC2023/DDDC2023.jl")
