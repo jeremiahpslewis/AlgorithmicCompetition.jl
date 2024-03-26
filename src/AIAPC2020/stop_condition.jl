22e40d230ef58a5af540994b9eb95429147e940f
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-03-26 16:37:23 +0100

    Fix stop condition

--- src/AIAPC2020/stop_condition.jl
+++ src/AIAPC2020/stop_condition.jl
@@ -18,12 +18,12 @@ end
 
 Returns a stop condition that stops when the environment has converged for all players.
 """
-function AIAPCStop(env::E; stop_on_convergence = true) where {E<:AbstractEnv}
+function AIAPCStop(env::E; stop_on_convergence::Bool = true) where {E<:AbstractEnv}
     stop_conditions = []
     push!(stop_conditions, StopAfterNEpisodes(env.max_iter, is_show_progress = false))
     if stop_on_convergence
         stop_converged = StopWhenConverged()
         push!(stop_conditions, stop_converged)
     end
-    return ComposedStopCondition(stop_conditions...)
+    return StopIfAny(stop_conditions...)
 end
