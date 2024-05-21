a126b666810822c65288dc547fac706cbcf59020
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-21 16:22:09 +0200

    Try different HPC / slurm approach

---
+++ src/hpc_slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=array_job
+#SBATCH --output=array_job_%A_%a.out
+#SBATCH --error=array_job_%A_%a.err
+#SBATCH --array=1-10
+# 000%1000
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:05:00
+#SBATCH -p normal
+#SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
+
+julia slurm_batch.jl
