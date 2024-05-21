0634701f019c286103f0885bb97c9f9d394b14aa
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-21 16:47:59 +0200

    Tweak params

--- src/slurm_batch.jl
+++ src/slurm_batch.jl
@@ -4,6 +4,6 @@ using Dates
 AlgorithmicCompetition.run_aiapc(
     version = "2024-05-21",
     start_timestamp = now(),
-    n_parameter_iterations = 10,
+    n_parameter_iterations = 10, # For full run, this is 1000
     parameter_index = parse(Int, ENV["SLURM_ARRAY_TASK_ID"]),
 )
