67763b208623119075a969fc8a8dfb51db41cd4e
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-21 22:40:06 +0200

    Bump memory

--- src/hpc_slurm.sh
+++ src/hpc_slurm.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=array_job_%A_%a.out
 #SBATCH --error=array_job_%A_%a.err
-#SBATCH --array=1-1000 # after % is the number of simultaneous jobs
-#SBATCH --mem-per-cpu=1G
+#SBATCH --array=233,248,258,259,265,273,284,288,295,296,302,333,339,900 # 1-1000 # after % is the number of simultaneous jobs
+#SBATCH --mem-per-cpu=2G
 #SBATCH --cpus-per-task=1
 #SBATCH --time=30:00:00 # For full run this is 4.5 hours
 #SBATCH -p normal
