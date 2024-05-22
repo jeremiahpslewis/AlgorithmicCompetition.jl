6c905579eca89194287b536426a984b2f587b822
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-22 10:38:31 +0200

    try lower mem limit

--- src/hpc_slurm.sh
+++ src/hpc_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=array_job_%A_%a.out
 #SBATCH --error=array_job_%A_%a.err
 #SBATCH --array=233,248,258,259,265,273,284,288,295,296,302,333,339,900 # 1-1000 # after % is the number of simultaneous jobs
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=1500M
 #SBATCH --cpus-per-task=1
 #SBATCH --time=30:00:00 # For full run this is 4.5 hours
 #SBATCH -p normal
