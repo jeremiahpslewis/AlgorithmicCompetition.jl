5ddddf67eac783982ce0fa180c5d3b971c4b1987
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-22 12:07:46 +0200

    Fix array param

--- src/hpc_slurm.sh
+++ src/hpc_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=array_job_%A_%a.out
 #SBATCH --error=array_job_%A_%a.err
-#SBATCH --array=233,248,258,259,265,273,284,288,295,296,302,333,339,900 # 1-1000 # after % is the number of simultaneous jobs
+#SBATCH --array=1:10 # 1-1000 # after % is the number of simultaneous jobs
 #SBATCH --mem-per-cpu=1500M
 #SBATCH --cpus-per-task=1
 #SBATCH --time=20:00:00 # For full run true value should be <13 hours
