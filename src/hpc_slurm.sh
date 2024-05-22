6021cafcb3ff0a8d01160ce37c62f219594f9819
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-22 11:15:57 +0200

    Tweak slurm script

--- src/hpc_slurm.sh
+++ src/hpc_slurm.sh
@@ -5,10 +5,12 @@
 #SBATCH --array=233,248,258,259,265,273,284,288,295,296,302,333,339,900 # 1-1000 # after % is the number of simultaneous jobs
 #SBATCH --mem-per-cpu=1500M
 #SBATCH --cpus-per-task=1
-#SBATCH --time=30:00:00 # For full run this is 4.5 hours
+#SBATCH --time=20:00:00 # For full run true value should be <13 hours
 #SBATCH -p normal
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
 
-export N_ITERATIONS=1000 # Number of iterations per parameter set; for full run, this is 1000
-export VERSION="2024-05-21-final"
+# Number of iterations per parameter set; for parallelized full run, 1 iteration over 10k parameters
+export N_ITERATIONS=1 
+
+export VERSION="2024-05-22"
 julia src/slurm_batch.jl
