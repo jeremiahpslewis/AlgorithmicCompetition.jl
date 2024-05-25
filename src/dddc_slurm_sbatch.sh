5d0dc6d484fe18fb131ba9d5207bd142617690db
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-25 23:56:32 +0200

    1.5 day time limit

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -5,7 +5,7 @@
 #SBATCH --array=1-1000 # 1-1000 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1000M
 #SBATCH --cpus-per-task=1
-#SBATCH --time=01:00:00 # For full run true value should be <12 hours
+#SBATCH --time=01:12:00 # For full run true value should be <12 hours
 #SBATCH -p normal
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
 
