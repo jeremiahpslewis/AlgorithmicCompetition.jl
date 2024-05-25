1e819e924c8595a278f846c6de6d6cf3906dd769
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-26 00:06:16 +0200

    Revert to 1500mb ram to avoid oom issues

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=log/array_job_%A_%a.out
 #SBATCH --error=log/array_job_%A_%a.err
 #SBATCH --array=1-1000 # 1-1000 # Number of iterations over all parameter sets
-#SBATCH --mem-per-cpu=1000M
+#SBATCH --mem-per-cpu=1500M
 #SBATCH --cpus-per-task=1
 #SBATCH --time=01:12:00 # For full run true value should be <12 hours
 #SBATCH -p normal
