4755606ab15cf2937c35f2e04b9c0a72b862fd5e
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-27 17:14:30 +0200

    Tweak  script

--- src/dddc_pc2_run.sh
+++ src/dddc_pc2_run.sh
@@ -13,7 +13,7 @@ cd AlgorithmicCompetition.jl || exit
 
 git pull
 
-julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate()'
+julia -e 'using Pkg; Pkg.activate("."); Pkg.update(); Pkg.instantiate()'
 
 mkdir -p log # Log directory for slurm task output
 sbatch src/dddc_slurm_sbatch.sh
