0385c73202b5ce6461a8d78b2db71cf50aee4b53
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-05-31 14:48:31 +0200

    Output parquet and jld2

--- src/DDDC2023/run_dddc.jl
+++ src/DDDC2023/run_dddc.jl
@@ -106,6 +106,7 @@ function run_dddc(;
     mkpath(folder_name)
     df = extract_sim_results(exp_list)
     jldsave(folder_name * ".jld2"; df)
+    jldsave(folder_name * ".parquet"; df)
     df = expand_and_extract_dddc(df)
     df_summary = construct_df_summary_dddc(df)
     Parquet2.writefile(folder_name * "df_summary.parquet", df_summary)
