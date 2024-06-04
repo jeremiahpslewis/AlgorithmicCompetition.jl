8868e44cf03b3fc7c28e68dcd8beda3e3e5ad841
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-06-04 08:53:22 +0200

    Drop time budget

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -5,7 +5,7 @@
 #SBATCH --array=1-100 # 1-1000 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1600M
 #SBATCH --cpus-per-task=100
-#SBATCH --time=2:30:00 # For full run true value should be <2 hours
+#SBATCH --time=1:30:00 # For full run true value should be <2 hours
 #SBATCH -p normal
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
 
