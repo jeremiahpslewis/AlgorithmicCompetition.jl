bfcbc9918548e66afc00f85164440e00b5c71d85
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-06-12 17:06:54 +0200

    Revert back to standard repo

--- src/run_pc2.sh
+++ src/run_pc2.sh
@@ -9,7 +9,7 @@ module load lang       # loading the gateway module
 module load JuliaHPC   # loading the latest JuliaHPC
 
 cd /scratch/hpc-prf-irddcc || exit
-export JULIA_DEPOT_PATH=/scratch/hpc-prf-irddcc/v2/.julia
+export JULIA_DEPOT_PATH=/scratch/hpc-prf-irddcc/.julia
 
 [ ! -d 'AlgorithmicCompetition.jl' ] && git clone https://github.com/jeremiahpslewis/AlgorithmicCompetition.jl.git
 
