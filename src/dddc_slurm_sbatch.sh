5dd48748aadb0f7d8ba98d8351036328ad21486d
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-06-17 17:54:47 +0200

    Expand grid

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
-#SBATCH --array=1-400 # Number of iterations over all parameter sets
+#SBATCH --array=1-500 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1600M
 #SBATCH --cpus-per-task=128
 #SBATCH --time=5:00:00 # For full run true value should be <2 hours
@@ -10,8 +10,8 @@
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
 
 # For full version, N_GRID_INCREMENTS=100
-export N_GRID_INCREMENTS=50
-export VERSION="2024-06-12-dddc-strong-weak-grid"
+export N_GRID_INCREMENTS=100
+export VERSION="2024-06-17-dddc-full-strong-weak-grid"
 export JULIA_DEPOT_PATH=/scratch/hpc-prf-irddcc/v1/.julia
 export DEBUG=0
 julia --project=. src/dddc_slurm_batch.jl
