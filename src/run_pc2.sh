238ef52536ca4a8d5f31e9f8b5fb80694199e611
Jeremiah Lewis <--get>
2024-08-12 15:47:28 +0200

    clean up script

--- src/run_pc2.sh
+++ src/run_pc2.sh
@@ -7,18 +7,13 @@
 
 module load lang       # loading the gateway module
 module load JuliaHPC   # loading the latest JuliaHPC
-# module load Julia/1.10.4 # This should be fine, not using MPI for this project...
-# curl -fsSL https://install.julialang.org | sh
-# . /pc2/users/i/irddcc1/.bashrc
 
 cd /scratch/hpc-prf-irddcc || exit
-# export JULIA_DEPOT_PATH=/scratch/hpc-prf-irddcc/.julia
 
 [ ! -d 'AlgorithmicCompetition.jl' ] && git clone https://github.com/jeremiahpslewis/AlgorithmicCompetition.jl.git
 
 cd AlgorithmicCompetition.jl || exit
 
-curl https://getmic.ro | bash # add micro text editor
 bash <(curl -Ls https://gist.githubusercontent.com/jeremiahpslewis/373e5e4d4d6faf1bf1a59ef9414019ca/raw/7e9166d3051687e2371b4ab98e6db1ac64432bef/sacct_tail.sh)
 
 git pull
