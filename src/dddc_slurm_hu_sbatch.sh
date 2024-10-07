ed29ed0d93c6e02cf7ce55c59fe8e7864035c253
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-10-07 12:46:08 +0200

    Fix singularity

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -15,12 +15,9 @@ export VERSION="2024-10-07-dddc-hu-test"
 export DEBUG=1
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
 
-module reset
-module load system singularity
-
 export APPTAINERENV_SLURM_ARRAY_TASK_ID="$SLURM_ARRAY_TASK_ID"
 export APPTAINERENV_SLURM_ARRAY_JOB_ID="$SLURM_ARRAY_JOB_ID"
 export APPTAINERENV_SLURM_CPUS_PER_TASK="$SLURM_CPUS_PER_TASK"
 
-singularity run algorithmiccompetition.jl_main.sif julia --project=/algcomp /algcomp/src/dddc_slurm_batch.jl
+apptainer run algorithmiccompetition.jl_main.sif julia --project=/algcomp /algcomp/src/dddc_slurm_batch.jl
 
