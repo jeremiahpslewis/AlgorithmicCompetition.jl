f8c71209b914796f7c94ace27eea9d87ccbb78ac
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-12-05 16:40:03 +0100

    Update SLURM script for increased array size and adjusted runtime parameters

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -2,16 +2,16 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
-#SBATCH --array=1-5 # Number of iterations over all parameter sets
+#SBATCH --array=1-1000 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1600M
 #SBATCH --cpus-per-task=64 # 128 core
-#SBATCH --time=16:00:00 # For full run true value should be <13 hours
+#SBATCH --time=2:00:00 # For full run true value should be <13 hours
 #SBATCH -p std
 
 # For full version, N_GRID_INCREMENTS=100
-export N_GRID_INCREMENTS=100 # Number of grid increments
+export N_GRID_INCREMENTS=20 # Number of grid increments
 export N_PARAMETER_ITERATIONS=1 # Number of iterations over all parameter sets per job
-export VERSION="2024-10-07-dddc-hu-test"
+export VERSION="2024-12-05-dddc-hu-test"
 export DEBUG=0
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
 
