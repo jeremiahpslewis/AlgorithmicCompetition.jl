1ab1da1c1789abd473e0f616f786fabcc5d4b211
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-12-12 15:26:10 +0100

    fix

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -2,15 +2,15 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
-#SBATCH --array=1-1000 # Number of iterations over all parameter sets
+#SBATCH --array=1-200 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1600M
-#SBATCH --cpus-per-task=64 # 128 core
+#SBATCH --cpus-per-task=16 # 128 core
 #SBATCH --time=2:00:00 # For full run true value should be <13 hours
 #SBATCH -p std
 
 # For full version, N_GRID_INCREMENTS=100
 export N_GRID_INCREMENTS=20 # Number of grid increments
-export N_PARAMETER_ITERATIONS=1 # Number of iterations over all parameter sets per job
+export N_PARAMETER_ITERATIONS=5 # Number of iterations over all parameter sets per job
 export VERSION="2024-12-05-dddc-hu-test"
 export DEBUG=0
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
