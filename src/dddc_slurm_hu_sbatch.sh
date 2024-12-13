011a8e614baabe06998553a9217080242b9cb94c
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-12-13 17:37:05 +0100

    Update SLURM script to increase job array iterations to 300 and reduce parameter iterations to 2

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
-#SBATCH --array=1-200 # Number of iterations over all parameter sets
+#SBATCH --array=1-300 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1600M
 #SBATCH --cpus-per-task=16 # 128 core
 #SBATCH --time=3:55:00 # For full run true value should be <13 hours
@@ -10,7 +10,7 @@
 
 # For full version, N_GRID_INCREMENTS=100
 export N_GRID_INCREMENTS=20 # Number of grid increments
-export N_PARAMETER_ITERATIONS=3 # Number of iterations over all parameter sets per job
+export N_PARAMETER_ITERATIONS=2 # Number of iterations over all parameter sets per job
 export VERSION="2024-12-05-dddc-hu-test"
 export DEBUG=0
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
