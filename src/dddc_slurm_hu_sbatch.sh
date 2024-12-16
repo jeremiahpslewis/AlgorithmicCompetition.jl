482cecf3c9ed4eadde22d0a75ccccd3e144e60c9
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-12-16 22:24:16 +0100

    fix batch params

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -2,16 +2,16 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
-#SBATCH --array=1-300 # Number of iterations over all parameter sets
+#SBATCH --array=1-1000 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1600M
-#SBATCH --cpus-per-task=16 # 128 core
-#SBATCH --time=3:55:00 # For full run true value should be <13 hours
-#SBATCH --partition standard
+#SBATCH --cpus-per-task=32 # 128 core
+#SBATCH --time=24:00:00 # For full run true value should be <13 hours
+#SBATCH --partition longrun
 
 # For full version, N_GRID_INCREMENTS=100
 export N_GRID_INCREMENTS=20 # Number of grid increments
-export N_PARAMETER_ITERATIONS=2 # Number of iterations over all parameter sets per job
-export VERSION="2024-12-05-dddc-hu-test"
+export N_PARAMETER_ITERATIONS=1 # Number of iterations over all parameter sets per job
+export VERSION="2024-12-16-dddc-dynamic-epsilon-greedy-beta"
 export DEBUG=0
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
 
