59501f1d2ce5dc94938a885f5b01719685e12711
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-12-23 13:12:46 -0600

    Add logging for summary statistics collection; update SLURM script resource allocations for efficiency

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -3,14 +3,14 @@
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
 #SBATCH --array=1-2 # Number of iterations over all parameter sets
-#SBATCH --mem-per-cpu=1600M
-#SBATCH --cpus-per-task=128 # 128 core
-#SBATCH --time=20:00:00 # For full run true value should be <13 hours
+#SBATCH --mem-per-cpu=3200M
+#SBATCH --cpus-per-task=10 # 128 core
+#SBATCH --time=4:00:00 # For full run true value should be <13 hours
 #SBATCH -p normal
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
 
 # For full version, N_GRID_INCREMENTS=100
-export N_GRID_INCREMENTS=10
+export N_GRID_INCREMENTS=4
 export N_PARAMETER_ITERATIONS=1 # Number of iterations over all parameter sets per job
 export VERSION="2024-12-23-dddc-full-test"
 export JULIA_DEPOT_PATH=/scratch/hpc-prf-irddcc/.julia
