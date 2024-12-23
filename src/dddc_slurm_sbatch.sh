ed446383e8b09d6fe5499becdcfeb35d42360986
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-12-23 09:24:01 -0600

    Update SLURM script parameters for testing and module loading

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
-#SBATCH --array=1-500 # Number of iterations over all parameter sets
+#SBATCH --array=1-2 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1600M
 #SBATCH --cpus-per-task=128 # 128 core
 #SBATCH --time=20:00:00 # For full run true value should be <13 hours
@@ -10,15 +10,16 @@
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
 
 # For full version, N_GRID_INCREMENTS=100
-export N_GRID_INCREMENTS=20
+export N_GRID_INCREMENTS=10
 export N_PARAMETER_ITERATIONS=1 # Number of iterations over all parameter sets per job
-export VERSION="2024-07-23-dddc-full-strong-weak-grid"
+export VERSION="2024-12-23-dddc-full-test"
 export JULIA_DEPOT_PATH=/scratch/hpc-prf-irddcc/.julia
 export DEBUG=0
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
 
 module reset
-module load system singularity
+module load lang       # loading the gateway module
+module load JuliaHPC   # loading the latest JuliaHPC
 
 export SINGULARITYENV_SLURM_ARRAY_TASK_ID="$SLURM_ARRAY_TASK_ID"
 export SINGULARITYENV_SLURM_ARRAY_JOB_ID="$SLURM_ARRAY_JOB_ID"
