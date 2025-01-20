1c29496d6944227748c15d0be7f8dc1922054cbf
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-01-20 18:28:43 +0100

    fix: update SLURM parameters and iteration counts for improved performance and data handling

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -4,15 +4,15 @@
 #SBATCH --error=log/%A_%a.err
 #SBATCH --array=1-2 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1600M
-#SBATCH --cpus-per-task=128 # 128 core
+#SBATCH --cpus-per-task=64 # 128 core
 #SBATCH --time=4:00:00 # For full run true value should be <13 hours
 #SBATCH -p normal
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
 
 # For full version, N_GRID_INCREMENTS=100
-export N_GRID_INCREMENTS=20
-export N_PARAMETER_ITERATIONS=1 # Number of iterations over all parameter sets per job
-export VERSION="2024-12-23-dddc-full-test"
+export N_GRID_INCREMENTS=50
+export N_PARAMETER_ITERATIONS=20 # Number of iterations over all parameter sets per job
+export VERSION="2025-01-20-dddc-revised-prices"
 export JULIA_DEPOT_PATH=/scratch/hpc-prf-irddcc/.julia
 export DEBUG=0
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
