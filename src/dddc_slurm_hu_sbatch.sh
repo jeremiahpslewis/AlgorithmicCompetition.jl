5aba73706f151a97aacdd47cfb51cea489dd2991
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-03-11 12:00:09 +0100

    feat: set JULIA_DEPOT_PATH for DDDC processing environment

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -13,6 +13,8 @@ export N_GRID_INCREMENTS=20 # Number of grid increments
 export N_PARAMETER_ITERATIONS=1 # Number of iterations over all parameter sets per job
 export VERSION="2025-03-10-7price-config"
 export DEBUG=1
+export JULIA_DEPOT_PATH=/lustre/wiwi/lewisjps/.julia
+
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
 
 module load julia
