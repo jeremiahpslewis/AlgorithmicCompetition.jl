7d573f0a4fee1c1f7551f00cc81fcb42dc87c090
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-03-17 18:12:34 +0100

    fix pc2 params

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -2,17 +2,17 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
-#SBATCH --array=1-2 # Number of iterations over all parameter sets
+#SBATCH --array=1-5 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=1600M
 #SBATCH --cpus-per-task=128 # 128 core
-#SBATCH --time=4:00:00 # For full run true value should be <13 hours
+#SBATCH --time=1:00:00 # For full run true value should be <13 hours
 #SBATCH -p normal
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
 
 # For full version, N_GRID_INCREMENTS=100
 export N_GRID_INCREMENTS=1
-export N_PARAMETER_ITERATIONS=500 # Number of iterations over all parameter sets per job
-export VERSION="2024-12-23-dddc-full-test"
+export N_PARAMETER_ITERATIONS=100 # Number of iterations over all parameter sets per job
+export VERSION="2025-03-17-dddc-trembling-hand"
 export JULIA_DEPOT_PATH=/scratch/hpc-prf-irddcc/.julia
 export DEBUG=0
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
