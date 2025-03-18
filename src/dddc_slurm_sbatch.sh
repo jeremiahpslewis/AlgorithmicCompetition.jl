8a37a6f09e426a391b69615cd4504e60c48fbac2
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-03-18 15:18:34 +0100

    feat: update SLURM batch script to change memory allocation to 1900M per CPU

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
 # SBATCH --array=1 # Number of iterations over all parameter sets
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=1900M
 #SBATCH --cpus-per-task=128 # 128 core
 #SBATCH --time=1:00:00 # For full run true value should be <13 hours
 #SBATCH -p normal
