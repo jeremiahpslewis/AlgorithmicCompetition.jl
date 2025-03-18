bf850d65719ee847e0422f7816f2ce78a8d6e78e
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-03-18 15:14:56 +0100

    feat: update SLURM batch script to reduce time limit and add express queue

--- src/dddc_slurm_sbatch.sh
+++ src/dddc_slurm_sbatch.sh
@@ -5,8 +5,9 @@
 # SBATCH --array=1 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=2G
 #SBATCH --cpus-per-task=128 # 128 core
-#SBATCH --time=4:00:00 # For full run true value should be <13 hours
+#SBATCH --time=1:00:00 # For full run true value should be <13 hours
 #SBATCH -p normal
+#SBATCH -q express
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de   # Where to send mail	
 
 # For full version, N_GRID_INCREMENTS=100
