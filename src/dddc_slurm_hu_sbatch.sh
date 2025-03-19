0b4c4816bf75b970bdd34a0994a34c4649fe60ca
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-03-19 17:32:17 +0100

    fix: update SBATCH array specification and increase N_PARAMETER_ITERATIONS to 50

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -2,17 +2,17 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
-#SBATCH --array=1 # Number of iterations over all parameter sets
+#SBATCH --array=1-20 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=3gb
 #SBATCH --cpus-per-task=64 # 128 core
-#SBATCH --time=1:00:00 # For full run true value should be <13 hours
+#SBATCH --time=1:20:00 # For full run true value should be <13 hours
 #SBATCH --partition=standard
 
 module load julia
 
 # For full version, N_GRID_INCREMENTS=100
 export N_GRID_INCREMENTS=0 # Number of grid increments
-export N_PARAMETER_ITERATIONS=1 # Number of iterations over all parameter sets per job
+export N_PARAMETER_ITERATIONS=50 # Number of iterations over all parameter sets per job
 export VERSION="2025-03-19-dddc-trembling-hand"
 export DEBUG=0
 export JULIA_DEPOT_PATH=/lustre/wiwi/lewisjps/.julia
