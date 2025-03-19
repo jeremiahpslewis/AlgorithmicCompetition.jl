b676f4a723fcc7829427ede16b1d288c44c24f0c
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-03-19 16:26:16 +0100

    fix: update SBATCH memory allocation and runtime in job script

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -3,18 +3,18 @@
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
 #SBATCH --array=1-2 # Number of iterations over all parameter sets
-#SBATCH --mem-per-cpu=1600M
+#SBATCH --mem-per-cpu=3000M
 #SBATCH --cpus-per-task=128 # 128 core
-#SBATCH --time=24:00:00 # For full run true value should be <13 hours
+#SBATCH --time=1:00:00 # For full run true value should be <13 hours
 #SBATCH --partition standard
 
 # For full version, N_GRID_INCREMENTS=100
 export N_GRID_INCREMENTS=20 # Number of grid increments
 export N_PARAMETER_ITERATIONS=1 # Number of iterations over all parameter sets per job
-export VERSION="2025-03-10-7price-config"
-export DEBUG=1
+export VERSION="2025-03-19-dddc-trembling-hand"
+export DEBUG=0
 export JULIA_DEPOT_PATH=/lustre/wiwi/lewisjps/.julia
-
+export LOG_DIR="/lustre/wiwi/lewisjps/AlgorithmicCompetition.jl/log"
 echo "Bash: Running DDDC with $N_GRID_INCREMENTS grid increments"
 
 module load julia
