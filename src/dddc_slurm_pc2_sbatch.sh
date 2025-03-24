08182d0a1ed3a4e3ac050886e63e3293eb4de6fa
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-03-24 16:35:04 +0100

    fix: update SLURM job parameters for CPU and runtime

--- src/dddc_slurm_pc2_sbatch.sh
+++ src/dddc_slurm_pc2_sbatch.sh
@@ -4,8 +4,8 @@
 #SBATCH --error=log/%A_%a.err
 #SBATCH --array=1-20 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=8gb
-#SBATCH --cpus-per-task=32 # 128 core
-#SBATCH --time=2:00:00 # For full run true value should be <13 hours
+#SBATCH --cpus-per-task=24 # 128 core
+#SBATCH --time=3:00:00 # For full run true value should be <13 hours
 #SBATCH -p normal
 #SBATCH --mail-user=irddcc1@mail.uni-paderborn.de # Where to send mail	
 
