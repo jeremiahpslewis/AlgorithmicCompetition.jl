2c476c1576b9e25185fc284ebe1a24b0c07dfb11
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-03-24 16:22:37 +0100

    fix: update SLURM job parameters for memory, CPU, and runtime

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
 #SBATCH --array=1-20 # Number of iterations over all parameter sets
-#SBATCH --mem-per-cpu=5gb
-#SBATCH --cpus-per-task=64 # 128 core
-#SBATCH --time=1:20:00 # For full run true value should be <13 hours
+#SBATCH --mem-per-cpu=6gb
+#SBATCH --cpus-per-task=32 # 128 core
+#SBATCH --time=4:20:00 # For full run true value should be <13 hours
 #SBATCH --partition=standard
 
 module load julia
