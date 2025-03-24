db9ff4142cb5da170357e59f7102ab81f1bbca80
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-03-24 18:11:37 +0100

    fix: remove redundant write_to_file_return_none argument in run_and_extract call

--- src/DDDC2023/run_dddc.jl
+++ src/DDDC2023/run_dddc.jl
@@ -131,7 +131,7 @@ function run_dddc(;
 
     write_to_file_path = mktempdir()
 
-    exp_output = @showprogress pmap(x -> run_and_extract(x, write_to_file_return_none=true, write_to_file_path=write_to_file_path, write_to_file_return_none=write_to_file_return_none), hyperparameter_vect; on_error = identity)
+    exp_output = @showprogress pmap(x -> run_and_extract(x, write_to_file_path=write_to_file_path, write_to_file_return_none=write_to_file_return_none), hyperparameter_vect; on_error = identity)
 
     if !write_to_file_return_none
         return exp_output
