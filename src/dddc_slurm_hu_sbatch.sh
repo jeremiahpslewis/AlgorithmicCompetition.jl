9017c3e513395801828ecf065b6b9da1d8f3ae78
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-06-19 14:11:07 +0200

    fix: adjust SLURM job parameters for proper execution and iteration count

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -2,17 +2,17 @@
 #SBATCH --job-name=array_job
 #SBATCH --output=log/%A_%a.out
 #SBATCH --error=log/%A_%a.err
-#SBATCH --array=1-5 # Number of iterations over all parameter sets
+#SBATCH --array=1 # Number of iterations over all parameter sets
 #SBATCH --mem-per-cpu=2gb
 #SBATCH --cpus-per-task=64 # 128 core
-#SBATCH --time=4:30:00 # For full run true value should be <13 hours
+#SBATCH --time=26:00:00 # For full run true value should be <13 hours
 #SBATCH --partition=standard
 
 module load julia
 
 # For full version, N_GRID_INCREMENTS=100
 export N_GRID_INCREMENTS=100 # Number of grid increments
-export N_PARAMETER_ITERATIONS=100 # Number of iterations over all parameter sets per job
+export N_PARAMETER_ITERATIONS=2 # Number of iterations over all parameter sets per job
 export VERSION="2025-06-19-dddc-state-action-tremble"
 export DEBUG=0
 export JULIA_DEPOT_PATH=/lustre/wiwi/lewisjps/.julia
