338d3d388a1968bb729aef5b32351307c975f06d
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2025-06-19 14:09:21 +0200

    fix: update N_GRID_INCREMENTS to 100 for proper grid configuration

--- src/dddc_slurm_hu_sbatch.sh
+++ src/dddc_slurm_hu_sbatch.sh
@@ -11,7 +11,7 @@
 module load julia
 
 # For full version, N_GRID_INCREMENTS=100
-export N_GRID_INCREMENTS=0 # Number of grid increments
+export N_GRID_INCREMENTS=100 # Number of grid increments
 export N_PARAMETER_ITERATIONS=100 # Number of iterations over all parameter sets per job
 export VERSION="2025-06-19-dddc-state-action-tremble"
 export DEBUG=0
